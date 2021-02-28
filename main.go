6a0f364bf021d0e34a8f043d032b5b5034249846
blmayer <bleemayer@gmail.com>
2021-02-28 15:43:06 -0300

    Added age

--- main.go
+++ main.go
@@ -11,12 +11,15 @@ type colours struct {
 	BgColour     string
 	BodyColour   string
 	BorderColour string
+	Age          int
 }
 
 var (
-	morningColours = colours{"AliceBlue", "DarkSlateBlue", "IndianRed"}
-	dayColours     = colours{"BlanchedAlmond", "Teal", "Salmon"}
-	nightColours   = colours{"#101010", "LawnGreen", "Fuchsia"}
+	morningColours = colours{"AliceBlue", "DarkSlateBlue", "IndianRed", ""}
+	dayColours     = colours{"BlanchedAlmond", "Teal", "Salmon", ""}
+	nightColours   = colours{"#101010", "LawnGreen", "Fuchsia", ""}
+
+	birth = time.Date(1992, 02, 19, 17, 00, 00, 0, time.UTC)
 
 	//go:embed static/*
 	pages embed.FS
@@ -38,15 +41,18 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		path = "index.html"
 	}
 
-	hour := time.Now().Hour()
+	now := time.Now()
 	colour := morningColours
-	if hour > 20 || hour < 5 {
+	if now.Hour() > 20 || now.Hour() < 5 {
 		colour = nightColours
-	} else if hour > 11 {
+	} else if now.Hour() > 11 {
 		colour = dayColours
 	} else {
 		colour = morningColours
 	}
+
+	years := now.Sub(birth).Hours() / (24 * 365.25)
+	colour.Age = int(years)
 
 	temp, err := template.ParseFS(pages, "static/*")
 	if err != nil {
