8fd09e50b3f51017dc8fffa12dd34efc739c19f9
blmayer <bleemayer@gmail.com>
2021-06-01 01:22:27 -0300

    Improved to work with folders

--- build.sh
+++ build.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+CWD=$(pwd)
+
+function process_file() {
+        for file in $@
+        do
+                echo "$file"
+                if [[ -d $file ]]
+                then
+                        echo "is dir"
+                        [ -d $CWD/web/$file ] || mkdir $CWD/web/$file
+                        process_file $file/*
+                else 
+                        fi=$(basename $file)
+                        echo -e "cat <<END_OF_TEXT\n$(cat $file)\nEND_OF_TEXT" | bash > $CWD/web/$file
+                fi
+        done
+}
 
 [ -e web/ ] || mkdir web
 cd static/
-for file in *
-do
-        fi=$(basename $file)
-        echo -e "cat <<END_OF_TEXT\n$(cat $file)\nEND_OF_TEXT" | bash > ../web/$fi
-done
+process_file *
